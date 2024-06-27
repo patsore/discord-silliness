000014c1a72d17e20928e4e7d60124a876fd3248
Ostap <80210497+patsore@users.noreply.github.com>
2024-06-27 20:31:35 +0300

    working sending requests

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@
 
 use std::net::{IpAddr, Ipv6Addr, SocketAddr, SocketAddrV6};
 use std::str::FromStr;
+use std::sync::Arc;
 use std::time::Duration;
 
 use http_body_util::{BodyExt, Empty};
 use hyper::body::{Buf, Bytes};
+use hyper::client::conn::http1::Builder;
 use hyper::Request;
+use hyper_rustls::ConfigBuilderExt;
 use hyper_util::client::legacy::connect::Connection;
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 use tokio::net::{TcpSocket, TcpStream};
 use tokio::sync::Semaphore;
 use tokio::task::JoinSet;
+use tokio_rustls::rustls::ClientConfig;
+use tokio_rustls::rustls::pki_types::ServerName;
+use tokio_rustls::TlsConnector;
 use tracing::{debug, error, info, warn};
 use tracing_subscriber;
-
-const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-";
+//Vanity invites are case-insensitive
+const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789-";
 const MAX_LENGTH: usize = 8;
 
 const IP_PREFIX: u128 = 0x20010470719200000000000000000000;
@@ -26,15 +32,15 @@ const PREFIX_SIZE: u8 = 48;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    static SEMA: Semaphore = Semaphore::const_new(200);
+    static SEMA: Semaphore = Semaphore::const_new(2000);
 
     info!("Starting the application");
 
     let mut set = JoinSet::new();
 
     for i in 0..=u16::MAX {
-        if i % 1 == 0 {
-            tokio::time::sleep(Duration::from_secs(3)).await;
+        if i % 10000 == 0 {
+            tokio::time::sleep(Duration::from_secs(1)).await;
         }
         set.spawn(async move {
             let ip_address = IP_PREFIX + ((i as u128) << 64) + 1;
@@ -53,11 +59,20 @@ async fn main() {
                 socket.set_freebind_ipv6(true).unwrap();
 
                 let tcp_socket = TcpSocket::from_std_stream(socket.into());
+
                 tcp_socket.set_keepalive(true).unwrap();
                 tcp_socket.set_reuseaddr(true).unwrap();
                 let sock_addr: SocketAddr = SocketAddr::new(IpAddr::from(ipv6_addr), (j + 1 - start) as u16);
                 tcp_socket.bind(sock_addr.into()).unwrap();
+
+                let provider = tokio_rustls::rustls::crypto::ring::default_provider();
+                let client_config = ClientConfig::builder().with_native_roots().unwrap().with_no_client_auth();
+                let connector = TlsConnector::from(Arc::new(
+                    client_config));
+
                 let stream = tcp_socket.connect(SocketAddr::new(IpAddr::from_str("2606:4700:3030::1").unwrap(), 443).into()).await.unwrap();
+                let mut stream = connector.connect(ServerName::try_from("discord.com").unwrap(), stream).await.unwrap();
+
                 let io = TokioIo::new(stream);
                 let (mut sender, conn) = match hyper::client::conn::http1::handshake(io).await {
                     Ok(conn) => conn,
@@ -76,11 +91,9 @@ async fn main() {
                 let mut invite_code = Vec::with_capacity(16);
                 encode_number_to_string(&mut invite_code, j);
                 let invite_code = unsafe { String::from_utf8_unchecked(invite_code) }.into_boxed_str();
-                let invite_link = format!("https://discord.com/api/v9/invite/{}", invite_code);
+                let invite_link = format!("https://discord.com/api/v9/invites/{}", invite_code);
 
-                let req = Request::builder()
-                    .method("GET")
-                    .uri(invite_link)
+                let req = Request::get(invite_link)
                     .header("Host", "discord.com")
                     .header("User-Agent", "curl/8.6.0")
                     .body(Empty::<Bytes>::new()).unwrap();
@@ -91,7 +104,7 @@ async fn main() {
                             info!("Found valid invite: {}", &invite_code);
                             valid_invites.push(invite_code);
                         } else {
-                            warn!("Invalid invite: {} - {:?} - {:?}", invite_code, &response.status(), String::from_utf8(response.into_body().collect().await.unwrap().to_bytes().into()).unwrap());
+                            // warn!("Invalid invite: {} - {:?} - {:?}", invite_code, &response.status(), String::from_utf8(response.into_body().collect().await.unwrap().to_bytes().into()).unwrap());
                         }
                     }
                     Err(e) => {
