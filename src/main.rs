19e4cf91146a34c8d05cfaa0124e7e3a897981e0
Ostap <80210497+patsore@users.noreply.github.com>
2024-07-09 12:37:36 +0300

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(ip_bits)]
 
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr, SocketAddrV6};
 use std::str::FromStr;
 use std::sync::Arc;
@@ -25,11 +27,27 @@ use tracing_subscriber;
 const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789-";
 const MAX_LENGTH: usize = 8;
 
-const IP_PREFIX: u128 = 0x20010470719200000000000000000000;
-const PREFIX_SIZE: u8 = 48;
+// const IP_PREFIX: u128 = ;
 
 #[tokio::main]
 async fn main() {
+    let ip_prefix: u128 = std::env::var("IP_PREFIX").expect("Couldn't find IP_PREFIX environment variable.").parse().expect("Couldn't parse IP_PREFIX as u128.");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_name = &args[1];
+    let mut file = match OpenOptions::new().write(true).create(true).open(file_name) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Failed to open file '{}': {}", file_name, e);
+            std::process::exit(1);
+        }
+    };
+
+
     tracing_subscriber::fmt::init();
 
     static SEMA: Semaphore = Semaphore::const_new(2000);
@@ -43,7 +61,7 @@ async fn main() {
             tokio::time::sleep(Duration::from_secs(1)).await;
         }
         set.spawn(async move {
-            let ip_address = IP_PREFIX + ((i as u128) << 64) + 1;
+            let ip_address = ip_prefix + ((i as u128) << 64) + 1;
             let ipv6_addr = Ipv6Addr::from_bits(ip_address);
             // info!("Sending requests from {:?}", ipv6_addr.to_string());
 
@@ -91,7 +109,7 @@ async fn main() {
                 let mut invite_code = Vec::with_capacity(16);
                 encode_number_to_string(&mut invite_code, j);
                 let invite_code = unsafe { String::from_utf8_unchecked(invite_code) }.into_boxed_str();
-                let invite_link = format!("https://discord.com/api/v9/invites/{}", invite_code);
+                let invite_link = format!("https://discord.com/api/v9/invites/{}?with_counts=true", invite_code);
 
                 let req = Request::get(invite_link)
                     .header("Host", "discord.com")
@@ -101,7 +119,7 @@ async fn main() {
                 match sender.send_request(req).await {
                     Ok(response) => {
                         if response.status().is_success() {
-                            info!("Found valid invite: {}", &invite_code);
+                            // info!("Found valid invite: {}", &invite_code);
                             valid_invites.push(invite_code);
                         } else {
                             // warn!("Invalid invite: {} - {:?} - {:?}", invite_code, &response.status(), String::from_utf8(response.into_body().collect().await.unwrap().to_bytes().into()).unwrap());
@@ -117,12 +135,13 @@ async fn main() {
         });
     }
 
-    let mut very_valid_invites = Vec::new();
-
-    while let Some(meow) = set.join_next().await {
-        match meow {
+    while let Some(task_output) = set.join_next().await {
+        match task_output {
             Ok(mut vec) => {
-                very_valid_invites.append(&mut vec);
+                println!("Task completed and joined.");
+                for invite in vec {
+                    file.write_all(format!("{}\n", invite).as_bytes()).expect("Failed to write to file");
+                }
             }
             Err(e) => {
                 error!("Task failed: {:?}", e);
@@ -130,7 +149,7 @@ async fn main() {
         }
     }
 
-    info!("Finished. Valid invites: {:?}", very_valid_invites);
+    info!("Done");
 }
 
 fn encode_number_to_string(target: &mut Vec<u8>, mut number: u32) {
